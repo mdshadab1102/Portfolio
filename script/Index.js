080db8ae4860c5fb27154afb24697595ee1dc0fa
Md Shadab <mdshadabharoon691@gmail.com>
2024-02-22 18:04:59 +0530

    Update projects

---
+++ script/Index.js
@@ -0,0 +1,29 @@
+let lightMode= localStorage.getItem('lightMode');
+const icon = document.querySelector('#icon');
+const enableLightMode=() =>{
+	document.body.classList.add('lightmode');
+	localStorage.setItem('lightMode','enabled');
+}
+const disableLightMode=() =>{
+	document.body.classList.remove('lightmode');
+	localStorage.setItem('lightMode',null);
+}
+if(lightMode === "enabled"){
+	enableLightMode();
+	icon.src="logo/moon.png";
+	plogo.src="logo/logolightmode.png"
+}
+
+icon.addEventListener("click", () =>{
+lightMode= localStorage.getItem('lightMode');
+if(lightMode !== "enabled"){
+	enableLightMode();
+	icon.src="logo/moon.png";
+	plogo.src="logo/logolightmode.png"
+}
+else{
+	disableLightMode();
+	icon.src="logo/sun.png";
+	plogo.src="logo/logolightmode2.png";
+}
+});
